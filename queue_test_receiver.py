c625a2ac534b76a906dd08ca75e2ecbdfe5e8b1d
Anders Wiklund <126193526+ycc140@users.noreply.github.com>
2023-07-17 01:30:09 +0200

    added new test program

---
+++ queue_test_receiver.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright: Wilde Consulting
+  License: Apache 2.0
+
+VERSION INFO::
+    $Repo: fastapi_celery
+  $Author: Anders Wiklund
+    $Date: 2023-07-17 00:51:13
+     $Rev: 28
+"""
+
+# BUILTIN modules
+import asyncio
+import contextlib
+
+# Local modules
+from src.config.setup import config
+from src.tools.rabbit_client import RabbitClient
+
+# Constants
+SERVICE = 'CallerService'
+
+
+# ---------------------------------------------------------
+#
+async def process_incoming_message(message: dict):
+    """ Print received message.
+
+    :param message:
+    """
+    print(f'Received: {message}')
+
+
+# ---------------------------------------------------------
+#
+async def receiver():
+    """ Start a asyncio task to consume messages. """
+    print(f'Started RabbitMQ message queue subscription on {SERVICE}...')
+    client = RabbitClient(config.rabbit_url, SERVICE, process_incoming_message)
+    connection = await asyncio.create_task(client.consume())
+
+    try:
+        # Wait until terminate
+        await asyncio.Future()
+
+    finally:
+        await connection.close()
+
+
+# ---------------------------------------------------------
+
+if __name__ == "__main__":
+
+    with contextlib.suppress(KeyboardInterrupt):
+        asyncio.run(receiver())
