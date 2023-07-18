1900bcf2af5ac76c1943e035e8d734fa30966388
Anders Wiklund <126193526+ycc140@users.noreply.github.com>
2023-07-18 10:05:27 +0200

    Add files via upload

--- src/tools/rabbit_client.py
+++ src/tools/rabbit_client.py
@@ -6,68 +6,42 @@ Copyright: Wilde Consulting
 VERSION INFO::
     $Repo: fastapi_celery
   $Author: Anders Wiklund
-    $Date: 2023-07-18 01:40:52
-     $Rev: 31
+    $Date: 2023-07-18 09:44:19
+     $Rev: 32
 """
 
 # BUILTIN modules
 import json
 import asyncio
-from typing import Callable
+from typing import Callable, Optional
 
 # Third party modules
 from aio_pika import connect, connect_robust, Message, DeliveryMode
 from aio_pika.abc import AbstractIncomingMessage, AbstractRobustConnection
 
-# Local modules
-from ..config.setup import config
-
-
-# ---------------------------------------------------------
-#
-async def publish_rabbit_message(message: dict, queue: str):
-    """ Publish message on specified RabbitMQ queue asynchronously.
-
-    :param message: Message to be sent.
-    :param queue: Message queue to use for message sending.
-    """
-    connection = await connect(config.rabbit_url)
-    channel = await connection.channel()
-
-    # Create message and publish it.
-    message_body = Message(
-        content_type='application/json',
-        delivery_mode=DeliveryMode.PERSISTENT,
-        body=json.dumps(message, ensure_ascii=False).encode())
-    await channel.default_exchange.publish(
-        routing_key=queue, message=message_body)
-
-    # Close the connection properly.
-    await connection.close()
-
 
 # -----------------------------------------------------------------------------
 #
-class RabbitConsumer:
-    """ This class handles asynchronous RabbitMQ queue subscriptions.
+class RabbitClient:
+    """ This class implements RabbitMQ Publish and Subscribe async handling.
 
-    Note: the queue mechanism is implemented to take advantage of good
-    horizontal message scaling when needed.
+    The RabbitMQ queue mechanism is used so that we can take advantage of
+    good horizontal message scaling when needed.
     """
-
-    rabbit_url = config.rabbit_url
 
     # ---------------------------------------------------------
     #
-    def __init__(self, service: str = None,
-                 incoming_message_handler: Callable = None):
+    def __init__(self, rabbit_url: str, service: Optional[str] = None,
+                 incoming_message_handler: Optional[Callable] = None):
         """ The class initializer.
 
-        :param service: Name of receiving service.
-        :param incoming_message_handler: Incoming message callback method.
+        :param rabbit_url: RabbitMQ's connection URL.
+        :param service: Name of message subscription queue.
+        :param incoming_message_handler: Received message callback method.
         """
 
         # Unique parameters.
+        self.rabbit_url = rabbit_url
         self.service_name = service
         self.message_handler = incoming_message_handler
 
@@ -85,7 +59,7 @@ class RabbitConsumer:
 
     # ---------------------------------------------------------
     #
-    async def consume(self) -> AbstractRobustConnection:
+    async def start_subscription(self) -> AbstractRobustConnection:
         """ Setup message listener with the current running asyncio loop. """
         loop = asyncio.get_running_loop()
 
@@ -101,7 +75,29 @@ class RabbitConsumer:
         # Creating a receive queue.
         queue = await channel.declare_queue(name=self.service_name, durable=True)
 
-        # Start consumption of existing and future messages.
+        # Start consuming existing and future messages.
         await queue.consume(self._process_incoming_message, no_ack=False)
 
         return connection
+
+    # ---------------------------------------------------------
+    #
+    async def publish_message(self, queue: str, message: dict):
+        """ Publish message on specified RabbitMQ queue asynchronously.
+
+        :param queue: Publishing queue.
+        :param message: Message to be published.
+        """
+        connection = await connect(url=self.rabbit_url)
+        channel = await connection.channel()
+
+        # Create message and publish it.
+        message_body = Message(
+            content_type='application/json',
+            delivery_mode=DeliveryMode.PERSISTENT,
+            body=json.dumps(message, ensure_ascii=False).encode())
+        await channel.default_exchange.publish(
+            routing_key=queue, message=message_body)
+
+        # Close the connection properly.
+        await connection.close()
