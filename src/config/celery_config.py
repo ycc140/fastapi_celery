f356e43a1d5b6a9761c69e94faf3dcb6f05985d3
Anders Wiklund <126193526+ycc140@users.noreply.github.com>
2023-08-27 14:54:56 +0200

    Activated two new parameters. Bumped version.

--- src/config/celery_config.py
+++ src/config/celery_config.py
@@ -6,8 +6,8 @@ Copyright: Wilde Consulting
 VERSION INFO::
     $Repo: fastapi_celery
   $Author: Anders Wiklund
-    $Date: 2023-07-24 19:41:02
-     $Rev: 41
+    $Date: 2023-08-27 14:34:48
+     $Rev: 46
 """
 
 # Local modules
@@ -21,7 +21,7 @@ broker_url = config.rabbit_url
 # Using the database to store task state and results.
 result_backend = f'{config.mongo_url}service_results'
 
-# Add input parameters to backend result.
+# Add input parameters to backend result (used by retry endpoint).
 result_extended = True
 
 # List of modules to import when the Celery
@@ -36,16 +36,16 @@ worker_task_log_format = '%(asctime)s | %(levelname)-8s | %(processName)s | ' \
 # Decrease the message rates substantially.
 worker_send_task_event = False
 
+# task messages will be acknowledged after the task has been executed,
+# not just before (the default behavior).
+task_acks_late = True
+
+# One worker takes 10 tasks from queue at a time
+# and will increase the performance.
+worker_prefetch_multiplier = 10
+
 ## task will be killed after 60 seconds
 # task_time_limit = 60
 
 ## task will raise exception SoftTimeLimitExceeded after 50 seconds.
 # task_soft_time_limit = 50
-
-## task messages will be acknowledged after the task has been executed,
-## not just before (the default behavior).
-# task_acks_late = True
-
-# One worker takes 10 tasks from queue at a time
-# and will increase the performance.
-worker_prefetch_multiplier = 10
